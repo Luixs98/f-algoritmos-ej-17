9f1af7ae1388201915a5cca6f161920d9ddcc7d2
Luixs98 <wlyfm2015@gmail.com>
2017-10-04 11:49:46 -0500

    Version 1.03

---
+++ clase1/tabla_de_multiplicar/src/tabla_de_multiplicar/Tabla_de_multiplicar.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tabla_de_multiplicar;
+
+/**
+ *
+ * @author dell
+ */
+public class Tabla_de_multiplicar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
